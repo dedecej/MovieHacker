b92a950b1e6953aea8e3c32e8d037bac8f069392
Jan Dedecek <dedecej@gmail.com>
2017-10-19 21:59:41 +0200

    Update for Swift4

--- MovieHacker/Movie.swift
+++ MovieHacker/Movie.swift
@@ -15,42 +15,31 @@ let dateFormatter = { () -> DateFormatter in
   return dateFormatter
 }()
 
-struct Movie
-{
+let decoder = { () -> JSONDecoder in
+  let decoder = JSONDecoder()
+  decoder.dateDecodingStrategy = .formatted(dateFormatter)
+  return decoder
+}()
+
+struct Movie: Codable {
   let title: String
-  let posterPath: String
+  let poster_path: String?
   let overview: String
-  let releaseDate: Date
+  private let release_date: Date?
 
-  // Parse movie from a json
-  init?(json: Any) {
-    // Try to cast json to dictionary and load its properties
-    guard let object = json as? [AnyHashable: Any],
-      let title = object["title"] as? String,
-      let posterPath = object["poster_path"] as? String,
-      let overview = object["overview"] as? String,
-      let releaseDate = dateFormatter.date(from: object["release_date"] as? String ?? "") else {
-        return nil
-    }
-
-    self.title = title
-    self.posterPath = posterPath
-    self.overview = overview
-    self.releaseDate = releaseDate
+  var releaseDate: Date? {
+    return release_date
   }
 
   var posterUrl: URL? {
-    return URL(string: "https://image.tmdb.org/t/p/w500/")?.appendingPathComponent(posterPath)
+    if let poster_path = poster_path {
+      return URL(string: "https://image.tmdb.org/t/p/w500/")?.appendingPathComponent(poster_path)
+    } else {
+      return nil
+    }
   }
 }
 
-func parse(json: Any) -> [Movie]?
-{
-  guard let results = (json as? [AnyHashable: Any])?["results"] as? [Any] else {
-    return nil
-  }
-
-  return results.flatMap {
-    return Movie(json: $0)
-  }
+struct MovieQueryResults: Codable {
+  let results: [Movie]
 }
